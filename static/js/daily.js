b9865200d4c7b991e2cd3a637cec19663e489132
Bullcaos <adrian.quigar@gmail.com>
2020-05-04 18:00:22 +0200

    Bug correction at daily view

--- static/js/daily.js
+++ static/js/daily.js
@@ -64,6 +64,7 @@ function leastSquares(values) {
     }
     pend_up = (values.length * sumxy) - (sumx * sumy);
     pend_down = (values.length * sumx2) - Math.pow(sumx, 2);
+    if(pend_down == 0) pend_down = 1;
     pend = pend_up / pend_down;
     base = sumy - (pend * sumx);
     base = base / values.length;
